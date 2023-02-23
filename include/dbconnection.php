c15e51218a98a084031a23a52aaa83ea65ba0698
criskhun <crispark30@gmail.com>
2023-02-23 20:06:31 +0800

    Update dbconnection.php

--- include/dbconnection.php
+++ include/dbconnection.php
@@ -1,8 +1,8 @@
 <?php  
-	$conn = mysql_connect('localhost', 'u398623434_reservation', 'Letmein#123');
+	$conn = mysql_connect('localhost', 'u854000491_lotreservation', 'Letmein#123');
 	 if (!$conn)
     {
 	 die('Could not connect: ' . mysql_error());
 	}
-	mysql_select_db("u398623434_reservation", $conn);
+	mysql_select_db("u854000491_lotreservation", $conn);
 ?>
