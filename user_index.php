c4c71912356c04b4fd98a667ba0c369c2e3444f3
Saycon <saycon.karl@gmail.com>
2023-05-29 23:38:43 +0800

    Update user_index.php

--- user_index.php
+++ user_index.php
@@ -32,6 +32,8 @@ require_once("include/auth.php");
   <li class="top"><a href="user_modelhouses.php" class="top_link"><span>Model Houses</span></a></li>
   <li class="top"><a href="requirements.php" class="top_link"><span>Requirements</span></a></li>
   <li class="top"><a href="user_contactus.php" class="top_link"><span>Contact Us</span></a></li>
+  <li class="top"><a href="payment.php" class="top_link"><span>Payment</span></a></li>
+  <li class="top"><a href="userpage.php" class="top_link"><span>Customer Page</span></a></li>
   <li class="top"><a href="userlogout.php" class="top_link"><span>Logout</span></a></li>
 
 </ul>
