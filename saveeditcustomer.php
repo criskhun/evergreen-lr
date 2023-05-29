f4b1b6672196f719e3ce99fdf373dd0af937ea41
Saycon <saycon.karl@gmail.com>
2023-05-30 02:40:18 +0800

    update code katulogon nako

--- saveeditcustomer.php
+++ saveeditcustomer.php
@@ -1,12 +1,12 @@
 <?php require("include/dbconnection.php"); 
 
 if(isset($_POST['save'])){
-
+$resid=$_POST['textid'];
 $buyer=$_POST['text1'];
 $address=$_POST['text2'];
 $email=$_POST['text3'];
 
-			mysql_query("UPDATE reserve Set  buyer='$buyer',address='$address',email='$email' where reserveid='$houseid'")
+			mysql_query("UPDATE reserve Set  buyer='$buyer',address='$address',email='$email' where reserveid='$resid'")
 												 																	
 				or die(mysql_error()); 
 	header("Location:customers.php");
