2d8c3afb70a754f3d354f52e49a3d8a3bfbcaeda
Saycon <saycon.karl@gmail.com>
2023-05-30 04:28:39 +0800

    Update savebalance.php

--- savebalance.php
+++ savebalance.php
@@ -3,23 +3,18 @@
 	session_start();
 	include("include/dbconnection.php");
 	
-$subname = $_POST['subname'];
-$phase = $_POST['phase'];
-$block =$_POST['block'];
-$lotarea =$_POST['lotarea'];
-$class =$_POST['class'];
-$price =$_POST['price'];
-$remarks =$_POST['remarks'];
-$lotstatus =$_POST['lotstatus'];
-$lotno =$_POST['lotno'];
-$tcp=$lotarea*$price;
+$buyer = $_POST['subname'];
+$baldate = $_POST['baldate'];
+$baldesc =$_POST['baldesc'];
+$amount =$_POST['price'];
 
-	$qry = "INSERT INTO lot(subdname,phase, block,lotno, lotarea,class, price,tcp,lotstatus, remarks) VALUES('$subname','$phase','$block','$lotno','$lotarea','$class','$price','$tcp','$lotstatus', '$remarks')";
+
+	$qry = "INSERT INTO balance(buyer,balance,baldesc,amount) VALUES('$buyer','$baldate','$baldesc','$amount')";
 	$result =mysql_query($qry);
 	
 	//Check whether the query was successful or not
 	if($result) {
-		header("location: addlot.php");
+		header("location: addbalance.php");
 		exit();
 	}else {
 		die("Query failed");
