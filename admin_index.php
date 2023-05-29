2ce7b9b8a8f7a9415b6d9c477a1f62626d201545
Saycon <saycon.karl@gmail.com>
2023-05-30 00:52:51 +0800

    supercode

--- admin_index.php
+++ admin_index.php
@@ -80,6 +80,7 @@ echo '<font size="2" color="black"><b>' . $rows. '</b></font>';
 	  <br />
 	  <a href="customers.php">Customer(s)</a><br /><br />	
 	  <a href="billing.php">Billing</a><br /><br />    </div>
+	  <a href="admin_payments.php">Payments</a><br /><br />    </div>
 	<br />
 </div>
 	  <br />
