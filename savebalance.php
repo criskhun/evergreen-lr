66990d7a1677c51717fd4cd475d8bd352bc31b65
Saycon <saycon.karl@gmail.com>
2023-05-30 04:37:18 +0800

    Update savebalance.php

--- savebalance.php
+++ savebalance.php
@@ -9,7 +9,7 @@ $baldesc =$_POST['baldesc'];
 $amount =$_POST['price'];
 
 
-	$qry = "INSERT INTO balance(buyer,balance,baldesc,amount) VALUES('$buyer','$baldate','$baldesc','$amount')";
+	$qry = "INSERT INTO balance(buyer,balancedate,baldesc,amount) VALUES('$buyer','$baldate','$baldesc','$amount')";
 	$result =mysql_query($qry);
 	
 	//Check whether the query was successful or not
