4f98918afaf627a0d3592e594c45542f212e5335
Saycon <saycon.karl@gmail.com>
2023-05-30 02:09:24 +0800

    Update deletependingpayments.php

--- deletependingpayments.php
+++ deletependingpayments.php
@@ -5,7 +5,7 @@ include("include/dbconnection.php");
 $picid=$_POST['pictureid'];
 
 
-mysql_query(" UPDATE payment SET statu = 'deleted' WHERE pictureid= '$picid'");
+mysql_query(" UPDATE payment SET status = 'deleted' WHERE pictureid= '$picid'");
 //mysql_query("UPDATE lot SET lotstatus = 'Temporarily Reserved' WHERE phase='$phase' and block='$block' and lotno='$lotno'");
 
 //mysql_query("UPDATE lot SET lotstatus = 'Available' WHERE subname='$subname' and phase='$phase' and block='$block' and lotno='$lotno'") ;
